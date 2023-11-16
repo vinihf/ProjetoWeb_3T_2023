f34c948790cf9d0d2e28148a4113dab98847b73d
Rato do Arroz <131390510+Ratatouille-87@users.noreply.github.com>
2023-11-16 10:13:15 -0300

    order by index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 include "db/database.php";
 
+// Variável para armazenar a ordem atual
+$ordenacao = isset($_GET['ordenacao']) ? $_GET['ordenacao'] : 'desc';
+
 $sql = "SELECT items.*, categoria.nome AS categoria_nome FROM items LEFT JOIN categoria ON items.id = categoria.iditems";
 $resultado = $conn->query($sql);
 $items = $resultado->fetch_all(MYSQLI_ASSOC);
@@ -12,22 +15,22 @@ $items2 = $resultado->fetch_all(MYSQLI_ASSOC);
 $jsonItems = json_encode($items);
 
 //Jogo com mais likes
-$sql_rankinglk = "SELECT r.item_id, COUNT(r.rating) AS dislike_count FROM ratings r WHERE r.rating = 'like' GROUP BY item_id ORDER BY dislike_count DESC LIMIT 1;";
+$sql_rankinglk = "SELECT r.item_id, COUNT(r.rating) AS dislike_count FROM ratings r WHERE r.rating = 'like' GROUP BY item_id ORDER BY dislike_count $ordenacao LIMIT 1;";
 $resultado = $conn->query($sql_rankinglk);
 $items4 = $resultado->fetch_all(MYSQLI_ASSOC);
 
 //Jogo com mais dislikes
-$sql_rankingdlk = "SELECT r.item_id, COUNT(r.rating) AS dislike_count FROM ratings r WHERE r.rating = 'dislike' GROUP BY item_id ORDER BY dislike_count DESC LIMIT 1";
+$sql_rankingdlk = "SELECT r.item_id, COUNT(r.rating) AS dislike_count FROM ratings r WHERE r.rating = 'dislike' GROUP BY item_id ORDER BY dislike_count $ordenacao LIMIT 1";
 $resultado = $conn->query($sql_rankingdlk);
 $items3 = $resultado->fetch_all(MYSQLI_ASSOC);
 
 //Jogos com mais likes
-$sql_likes = "SELECT r.item_id, COUNT(r.rating) AS dislike_count FROM ratings r WHERE r.rating = 'liiike' GROUP BY item_id ORDER BY dislike_count DESC";
+$sql_likes = "SELECT r.item_id, COUNT(r.rating) AS dislike_count FROM ratings r WHERE r.rating = 'liiike' GROUP BY item_id ORDER BY dislike_count $ordenacao";
 $resultado = $conn->query($sql_likes);
 $items_like = $resultado->fetch_all(MYSQLI_ASSOC);
 
 //Jogo com mais dislikes
-$sql_dislikes = "SELECT r.item_id, COUNT(r.rating) AS dislike_count FROM ratings r WHERE r.rating = 'dislike' GROUP BY item_id ORDER BY dislike_count DESC";
+$sql_dislikes = "SELECT r.item_id, COUNT(r.rating) AS dislike_count FROM ratings r WHERE r.rating = 'dislike' GROUP BY item_id ORDER BY dislike_count $ordenacao";
 $resultado = $conn->query($sql_dislikes);
 $items_dislike = $resultado->fetch_all(MYSQLI_ASSOC);
 ?>
@@ -44,6 +47,13 @@ $items_dislike = $resultado->fetch_all(MYSQLI_ASSOC);
         <h1>Rate The Game</h1>
         <a href="logout.php"><input type="button" value="Logout" name="logout"></a>
     </header>
+    
+    <label for="ordenacao">Ordem:</label>
+    <select id="ordenacao" name="ordenacao" onchange="atualizarOrdenacao()">
+        <option value="desc" <?php echo ($ordenacao == 'desc') ? 'selected' : ''; ?>>Decrescente</option>
+        <option value="asc" <?php echo ($ordenacao == 'asc') ? 'selected' : ''; ?>>Crescente</option>
+    </select>
+
     <?php if($_SESSION['tipo'] == "user" or $_SESSION['tipo'] == "manager") : ?>
     
         <?php if($items2 == null) : ?>
@@ -71,41 +81,39 @@ $items_dislike = $resultado->fetch_all(MYSQLI_ASSOC);
         
         <?php endif; ?>
 
-
         <?php foreach($items as $item) : ?>
-    <div class="box">
-        <img src="<?php echo $item['image_url']; ?>" alt="" width="250px">
-        <p><?php echo $item['name']; ?></p>
-        <p> Categoria: <?php echo $item['categoria_nome']; ?></p>
-        <?php if ($_SESSION['tipo'] == "user" ) : ?>
-            <!-- Botões de avaliação para usuários -->
-            
-    <a href="processar_rating.php?id=<?php echo $item['id_item']; ?>&&tipo=1"><input type="button" value="✅"></a>
-    <a href="processar_rating.php?id=<?php echo $item['id_item']; ?>&&tipo=2"><input type="button" value="❌"></a>
-   
-        <?php endif; ?>
-    </div>
-<?php endforeach; ?>
+            <div class="box">
+                <img src="<?php echo $item['image_url']; ?>" alt="" width="250px">
+                <p><?php echo $item['name']; ?></p>
+                <p> Categoria: <?php echo $item['categoria_nome']; ?></p>
+                <?php if ($_SESSION['tipo'] == "user" ) : ?>
+                    <!-- Botões de avaliação para usuários -->
+                    <a href="processar_rating.php?id=<?php echo $item['id_item']; ?>&&tipo=1"><input type="button" value="✅"></a>
+                    <a href="processar_rating.php?id=<?php echo $item['id_item']; ?>&&tipo=2"><input type="button" value="❌"></a>
+                <?php endif; ?>
+            </div>
+        <?php endforeach; ?>
 
     <?php endif;?>
 
     <?php if($_SESSION['tipo'] == 'manager') :?>
-
         <?php foreach($items as $item1) : ?>
             <a href="editar_jogo.php?id=<?php echo $item1['id'];?>"><img src="<?php echo $item1['image_url'] ?>" alt="">
             <p><?php echo $item1['name']; ?></p>
             </a>
         <?php endforeach;?>
         <a href="cadastrar_jogo.php">Cadastrar Jogo</a>
-
     <?php endif;?>
-
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-
 <script>
     
+    function atualizarOrdenacao() {
+    const ordenacao = document.getElementById("ordenacao").value;
+            window.location.href = "index.php?ordenacao=" + ordenacao;
+        }
+
 $(document).ready(function() {
     let currentIndex = 0;
     const items = <?php echo $jsonItems; ?>;
